898c50a396c13cd59483c6724814a7d15f91767a
Frank-Lavoie <66792121+Frank-Lavoie@users.noreply.github.com>
2025-01-22 14:59:17 -0500

    Step 7

--- Exercice_01/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Exercice_01/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -34,14 +34,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message : String, modifier: Modifier = Modifier){
+fun GreetingText(message : String, from : String, modifier: Modifier = Modifier){
     Text(text=message, fontSize=100.sp, lineHeight = 116.sp)
+    Text(text=from, fontSize=36.sp)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday Sam!")
+        GreetingText("Happy Birthday Sam!", from = "From Emma")
     }
 }
