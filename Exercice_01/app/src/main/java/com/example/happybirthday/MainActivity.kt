e067c12bf6746df7e99b38bfb489751a9d4dc938
Frank-Lavoie <66792121+Frank-Lavoie@users.noreply.github.com>
2025-01-23 14:01:13 -0500

    Step 7

--- Exercice_01/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Exercice_01/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,8 +39,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma")
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -86,6 +88,8 @@ fun GreetingImage(message : String, from : String, modifier : Modifier = Modifie
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage("Happy Birthday Sam!", from = "From Emma")
+        GreetingImage(
+            stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text))
     }
 }
